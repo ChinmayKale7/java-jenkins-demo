059801bacb5c2256454a2927824a118ea652724d
ChinmayKale7 <166749536+ChinmayKale7@users.noreply.github.com>
2026-02-05 15:32:24 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Hello from Jenkins 🚀");
+
+        int a = 15;
+        int b = 25;
+
+        System.out.println("Addition = " + (a + b));
+
+    }
+}
